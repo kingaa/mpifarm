3c7e98f0c07100ee29e6dd037a8d213e754f786c
Aaron A. King <kingaa@umich.edu>
2006-12-20 02:44:49 +0000

    renamed "mpi.farm"
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mpifarm@14 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/R/mpifarm.R
+++ trunk/R/mpifarm.R
@@ -1,57 +1,32 @@
-pdapply.slave <- function (fn, common=list()) { # slave procedure for pdapply
-  fun <- parse(text=fn)
-  wrap.fn <- function (x) eval(fun,envir=x)
-  go <- TRUE
-  attach(common,warn.conflicts=FALSE)
-  while (go) {
-    pars <- mpi.recv.Robj(source=0,tag=mpi.any.tag())
-    if (mpi.get.sourcetag()[2] == 3) {
-      result <- try(
-                    cbind(before=pars,after=wrap.fn(pars)),
-                    silent=T
-                    )
-      if (inherits(result,'try-error')) {
-        tag <- 66
-      } else {
-        tag <- 33
-      }
-      mpi.send.Robj(result,dest=0,tag=tag)
-    } else {
-      go <- FALSE                       # terminate
-    }
-  }
-  detach(common)
-}
-
-## row-by-row data.frame apply (parallel version)
-pdapply <- function (job, pars, common = list(), info = TRUE) {
+## farm out a bunch of jobs
+mpi.farm <- function (proc, joblist, common=list(), info=TRUE) {
   if (mpi.comm.size() < 2)
-    stop("pdapply: no slaves running")
-  mpi.bcast.Robj2slave(pdapply.slave)
+    stop("mpi.farm: no slaves running")
+  mpi.bcast.Robj2slave(mpi.farm.slave)
   mpi.bcast.cmd(if(!exists('.Random.seed')) runif(1))
-  fn <- deparse(substitute(job))
-  mpi.remote.exec(pdapply.slave,fn,common,ret=FALSE)
-  result <- data.frame()
+  fn <- deparse(substitute(proc))
+  mpi.remote.exec(mpi.farm.slave,fn,common,ret=FALSE)
+  result <- vector('list',length(joblist))
   sent <- 0
   rcvd <- 0
   nslave <- mpi.comm.size() - 1
   slaveinfo <- matrix(0,1,nslave+1,dimnames=list('jobs',c(1:nslave,'total')))
   for (d in 1:nslave) {  # initialize the queue
     sent <- sent+1
-    if (sent <= nrow(pars)) {
-      mpi.send.Robj(pars[sent,],dest=d,tag=3)
+    if (sent <= length(joblist)) {
+      mpi.send.Robj(joblist[[sent]],dest=d,tag=3)
     } else {
       mpi.send.Robj(0,dest=d,tag=666)
     }
   }
-  while (rcvd < nrow(pars)) {
+  while (rcvd < length(joblist)) {
     res <- mpi.recv.Robj(source=mpi.any.source(),tag=mpi.any.tag())
     srctag <- mpi.get.sourcetag()
     src <- mpi.get.sourcetag()[1]
     tag <- mpi.get.sourcetag()[2]
     rcvd <- rcvd+1
     if (tag == 33) {
-      result <- rbind(result,res)
+      result[[rcvd]] <- res
       slaveinfo[1,src] <- slaveinfo[1,src] + 1
       slaveinfo[1,nslave+1] <- slaveinfo[1,nslave+1] + 1
       if (info)
@@ -62,9 +37,9 @@ pdapply <- function (job, pars, common = list(), info = TRUE) {
       else
         warning('slave ',format(src),' reports: ',res)
     }
-    if (sent < nrow(pars)) {
+    if (sent < length(joblist)) {
       sent <- sent+1
-      mpi.send.Robj(pars[sent,],dest=src,tag=3)
+      mpi.send.Robj(joblist[[sent]],dest=src,tag=3)
     } else {
       mpi.send.Robj(0,dest=src,tag=666)
     }
@@ -72,7 +47,7 @@ pdapply <- function (job, pars, common = list(), info = TRUE) {
   result
 }
 
-plapply.slave <- function (fn, common=list()) { # slave procedure for plapply
+mpi.farm.slave <- function (fn, common=list()) { # slave procedure for mpi.farm
   fun <- parse(text=fn)
   wrap.fn <- function (x) eval(fun,envir=x)
   go <- TRUE
@@ -97,67 +72,19 @@ plapply.slave <- function (fn, common=list()) { # slave procedure for plapply
   detach(common)
 }
 
-plapply <- function (job, pars, common=list(), info=TRUE) { # parallel lapply
-  if (mpi.comm.size() < 2)
-    stop("plapply: no slaves running")
-  mpi.bcast.Robj2slave(plapply.slave)
-  mpi.bcast.cmd(if(!exists('.Random.seed')) runif(1))
-  fn <- deparse(substitute(job))
-  mpi.remote.exec(plapply.slave,fn,common,ret=FALSE)
-  result <- vector('list',length(pars))
-  sent <- 0
-  rcvd <- 0
-  nslave <- mpi.comm.size() - 1
-  slaveinfo <- matrix(0,1,nslave+1,dimnames=list('jobs',c(1:nslave,'total')))
-  for (d in 1:nslave) {  # initialize the queue
-    sent <- sent+1
-    if (sent <= length(pars)) {
-      mpi.send.Robj(pars[[sent]],dest=d,tag=3)
-    } else {
-      mpi.send.Robj(0,dest=d,tag=666)
-    }
-  }
-  while (rcvd < length(pars)) {
-    res <- mpi.recv.Robj(source=mpi.any.source(),tag=mpi.any.tag())
-    srctag <- mpi.get.sourcetag()
-    src <- mpi.get.sourcetag()[1]
-    tag <- mpi.get.sourcetag()[2]
-    rcvd <- rcvd+1
-    if (tag == 33) {
-      result[[rcvd]] <- res
-      slaveinfo[1,src] <- slaveinfo[1,src] + 1
-      slaveinfo[1,nslave+1] <- slaveinfo[1,nslave+1] + 1
-      if (info)
-        print(slaveinfo)
-    } else {
-      if (info)
-        message('slave ',format(src),' reports: ',res)
-      else
-        warning('slave ',format(src),' reports: ',res)
-    }
-    if (sent < length(pars)) {
-      sent <- sent+1
-      mpi.send.Robj(pars[[sent]],dest=src,tag=3)
-    } else {
-      mpi.send.Robj(0,dest=src,tag=666)
-    }
-  }
-  result
-}
-
-## row-by-row data.frame apply (serial version)
-sdapply <- function (job, pars, common=list(), info = TRUE) { 
-  fn <- substitute(job)
-  result <- data.frame()
-  wrap.fn <- function (x) eval(fn,envir=x)
+## serial version
+farm <- function (proc, joblist, common=list(), info = TRUE) { 
+  if (!exists('.Random.seed')) runif(1)
+  fun <- substitute(proc)
+  result <- vector('list',length(joblist))
   attach(common)
-  for (k in 1:nrow(pars)) {
+  for (j in 1:length(joblist)) {
     res <- try(
-               cbind(before=pars[k,],after=wrap.fn(pars[k,])),
+               eval(fun,envir=joblist[[j]]),
                silent=T
                )
     if (!(inherits(res,'try-error'))) {
-      result <- rbind(result,res)
+      result[[j]] <- res
     } else {
       if (info)
         message('sdapply reports: ', res)
