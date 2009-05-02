118a72b884b67571cc29139556f13254724b626b
Aaron A. King <kingaa@umich.edu>
2009-05-02 13:52:26 +0000

    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mpifarm@28 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/inst/scripts/miffit.R
+++ trunk/inst/scripts/miffit.R
@@ -3,14 +3,14 @@
 ## a script for running multiple MIFs using Rmpi and mpifarm
 
 ## set some default values
-nreps <- 2         # number of replicate MIFs to run per parameter set
-nmifs <- 80                       # total number of MIFs per replicate
+nreps <- 1         # number of replicate MIFs to run per parameter set
+nmifs <- 20                       # total number of MIFs per replicate
 unweighted <- 0                  # number of unweighted MIF iterations
 max.fail <- 100 # maximum number of filtering failures before an error is triggered
-gran <- 4                               # granularity (MIFs per chunk)
+gran <- 5                               # granularity (MIFs per chunk)
 
 nparticles <- 10000                     # pfilter's Np
-cooling <- 0.95                         # cooling factor
+cooling <- 0.99                         # cooling factor
 ic.lag <- 60                            # fixed lag for IVP estimation
 var.factor <- 4                         # MIF's variance factor
 init.disp <- 0           # initial displacement of starting conditions
@@ -21,8 +21,9 @@ nfilters <- 10 # number of independent particle filter runs used to estimate the
 jobname <- NULL           # this must be set by a commandline argument
 scratchdir <- paste("/scratch/",system("whoami",intern=T),"/",sep="") # scratch directory
 
-modelRfile <- NULL # file where 'transform.fn' and 'make.pomp' are defined
-solib <- NULL # if this is set to the stem-name of the SO library, the library will be loaded
+modelRfile <- "cholmodel.R" # file where 'transform.fn' and 'make.pomp' are defined
+modelCfile <- "cholmodel.c" # file where 'transform.fn' and 'make.pomp' are defined
+solib <- "cholmodel.so" # if this is set to the stem-name of the SO library, the library will be loaded
 
 ## get, parse, and evaluate the command-line arguments
 eval(parse(text=commandArgs(trailingOnly=TRUE)))
@@ -30,11 +31,18 @@ eval(parse(text=commandArgs(trailingOnly=TRUE)))
 if (is.null(jobname))
   stop("you must supply a ",sQuote("jobname"))
 
-if (!is.null(solib) && !file.exists(solib))
-  stop("file ",sQuote(solib)," not found")
-
 if (!file.exists(modelRfile))
   stop("file ",sQuote(modelRfile)," not found")
+
+rexe <- paste(R.home(),"bin/R",sep="/")
+pomp.inc <- system.file("include/pomp.h",package="pomp")
+pomp.lib <- system.file("libs/pomp.so",package="pomp")
+system(paste("cp",pomp.inc,".",sep=" "))
+system(paste(rexe,"CMD SHLIB -o",solib,modelCfile,pomp.lib,sep=" "))
+system("rm pomp.h")
+
+if (!is.null(solib) && !file.exists(solib))
+  stop("file ",sQuote(solib)," not found")
 
 ## the following cannot be changed by commandline arguments
 id <- paste(basename(getwd()),jobname,sep='_') # an identifier for the files to be saved
@@ -44,15 +52,11 @@ bestfile <- paste('best_',jobname,'.csv',sep='') # CSV file to store MLEs
 mlefile <- paste('mle_',jobname,'.rda',sep='') # binary file to store MLEs
 
 require(Rmpi)
-mpi.spawn.Rslaves(needlog=F)
 require(mpifarm)
-
 require(pomp)
 
-if (!file.exists(modelRfile))
-  stop("file ",sQuote(modelRfile)," not found")
 source(modelRfile)         # model-specific codes, loading data, etc.
-if (!(exists("transform.fn")&&exists("make.pomp")))
+if (!(exists("par.trans")&&exists("par.untrans")&&exists("make.pomp")))
   stop("file ",sQuote(modelRfile)," does not define ",sQuote("transform.fn")," and ",sQuote("make.pomp")," as it should")
 
 if (file.exists(imagefile)) {   # we are continuing where we left off?
@@ -89,47 +93,38 @@ if (file.exists(imagefile)) {   # we are continuing where we left off?
   joblist <- vector(mode='list',length=njobs)
   count <- 0
   for (d in seq(length=ndsets)) {
-    theta.guess <- unlist(parameters[d,]) # extract the starting (guess) parameters
-    sig <- sigma[is.finite(theta.guess)&(sigma>0)] # the random-walk SDs
-    theta.guess <- theta.guess[is.finite(theta.guess)] # only finite guesses play any role
-    par.trans <- transform.fn(model=models[d],dir='transform') # transform parameters
-    theta.guess <- par.trans(theta.guess)
-    theta.x <- matrix(
-                      data=theta.guess,
-                      ncol=nreps,
-                      nrow=length(theta.guess),
-                      dimnames=list(names(theta.guess),NULL)
-                      )
-    ## add some random variability to explore the parameter space
-    theta.x[names(sig),] <- rnorm(
-                                    n=nreps*length(sig),
-                                    mean=theta.guess[names(sig)],
-                                    sd=init.disp*sig
-                                    )
     po <- make.pomp(dataset=datasets[d],model=models[d])               
+    sigma <- sigma[sigma>0] # the random-walk SDs
+    parnames <- names(sigma)
+    ivpnames <- grep(glob2rx("*.0"),parnames,val=T)
+    parnames <- parnames[!(parnames%in%ivpnames)]
+    guess.params <- unlist(parameters[d,])
+    theta.x <- matrix(
+                      data=rnorm(
+                        n=nreps*length(guess.params),
+                        mean=par.trans(po,guess.params),
+                        sd=init.disp*sigma
+                        ),
+                      ncol=nreps,
+                      nrow=length(guess.params),
+                      dimnames=list(names(guess.params),NULL)
+                      )
 
     for (r in seq(length=nreps)) {
       count <- count+1
-      ivpnames <- grep('\\.0$',names(sig),value=T,perl=T)
-      estnames <- names(sig)[!(names(sig)%in%ivpnames)]
-      set.seed(seeds[count])
-      rngstate <- .Random.seed
       joblist[[count]] <- list(
                                mle=mif(
                                  po,
                                  Nmif=0,
                                  start=theta.x[,r],
                                  ivps=ivpnames,
-                                 pars=estnames,
-                                 rw.sd=sig,
-                                 alg.pars=list(
-                                   Np=nparticles,
-                                   cooling.factor=cooling,
-                                   ic.lag=ic.lag,
-                                   var.factor=var.factor
-                                   )
+                                 pars=parnames,
+                                 rw.sd=sigma,
+                                 Np=nparticles,
+                                 cooling.factor=cooling,
+                                 ic.lag=ic.lag,
+                                 var.factor=var.factor
                                  ),
-                               rngstate=rngstate,
                                seed=seeds[count],
                                model=models[d],
                                dataset=datasets[d],
@@ -141,20 +136,28 @@ if (file.exists(imagefile)) {   # we are continuing where we left off?
   save(list='joblist',file=imagefile)
 }
 
+mpi.spawn.Rslaves(needlog=F)
+
 tic <- Sys.time()
 
-joblist <- mpi.farm(
+results <- mpi.farm(
                     { # this is the expression to be evaluated on each slave
                       require(pomp)
                       ## name of a file in which to save checkpoints
                       ckptfile <- file.path(scratchdir,paste(dataset,'_',id,'_',done,'_',seed,'.rda',sep=''))
                       if (!is.null(solib)) dyn.load(solib) # load the SO library
+                      if (done == 0) {  # first MIF runs
+                        save.seed <- .Random.seed
+                        set.seed(seed)
+                        rngstate <- .Random.seed
+                        .Random.seed <<- save.seed
+                      }
                       if (done < nmifs) { # continue MIFfing
                         save.seed <- .Random.seed
                         .Random.seed <<- rngstate
                         mle <- continue(
                                         mle,
-                                        Nmif=gran,
+                                        Nmif=min(gran,nmifs-done),
                                         weighted=(done>unweighted),
                                         warn=FALSE,
                                         max.fail=max.fail
@@ -163,17 +166,19 @@ joblist <- mpi.farm(
                         nfail <- conv.rec(mle,'nfail')
                         nfail <- nfail[length(nfail)-1]
                         loglik <- logLik(mle)
+                        rngstate <- .Random.seed
                         .Random.seed <<- save.seed
                         all.done <- FALSE
                       } else if (filter.q) { # final particle filtering
                         save.seed <- .Random.seed
                         .Random.seed <<- rngstate
                         ff <- lapply(
-                                     1:nfilters,
-                                     function(n)pfilter(mle,max.fail=1000)
+                                     seq(length=nfilters),
+                                     function(n)pfilter(mle,max.fail=max.fail)
                                      )
                         nfail <- sapply(ff,function(x)x$nfail)
                         loglik <- sapply(ff,function(x)x$loglik)
+                        rngstate <- .Random.seed
                         .Random.seed <<- save.seed
                         all.done <- TRUE
                       } else {          # nothing to do
@@ -212,46 +217,52 @@ joblist <- mpi.farm(
 
 toc <- Sys.time()
 print(toc-tic)
-save(list='joblist',file=imagefile)
-
-## collate the results for storage in a CSV file
-x <- Reduce(
-            function(x,y)merge(x,y,all=T),
-            lapply(
-                   joblist,
-                   function (x) {
-                     par.untrans <- transform.fn(model=x$model,dir='untransform')
-                     cbind(
-                           data.frame(
-                                      model=x$model,
-                                      dataset=x$dataset
-                                      loglik=mean(x$loglik),
-                                      loglik.sd=sd(x$loglik),
-                                      nfail.max=max(x$nfail),
-                                      nfail.min=min(x$nfail)
-                                      ),
-                           as.list(par.untrans(coef(x$mle)))
-                           )
-                   }
-                   )
-            )
-
-## extract the best likelihood for each dataset/model combination
-best.ind <- tapply(1:nrow(x),list(x$dataset,x$model),function(k)k[which.max(x$loglik[k])])
-best <- x[best.ind,]
-best <- best[order(as.character(best$dataset)),]
-write.csv(best,file=bestfile,row.names=FALSE,na="")
-
-## also save the MIF objects in a binary file
-mle <- lapply(
-              joblist[best.ind],
-              function(x)x$mle
-              )
-names(mle) <- sapply(joblist[best.ind],function(x)x$dataset)
-mle <- mle[order(names(mle))]
-save(list='mle',file=mlefile)
 
 mpi.close.Rslaves()
 mpi.exit()
+
+noerr <- !sapply(results,function(x)inherits(x,'try-error'))
+joblist <- results[noerr]
+print(results[!noerr])
+
+save(list=c('joblist','results'),file=imagefile)
+
+## collate the results for storage in a CSV file
+if (filter.q) {
+  x <- Reduce(
+              function(x,y)merge(x,y,all=T),
+              lapply(
+                     results[noerr],
+                     function (x) {
+                       cbind(
+                             data.frame(
+                                        model=x$model,
+                                        dataset=x$dataset,
+                                        loglik=mean(x$loglik),
+                                        loglik.sd=sd(x$loglik),
+                                        nfail.max=max(x$nfail),
+                                        nfail.min=min(x$nfail)
+                                        ),
+                             as.list(par.untrans(x$mle,coef(x$mle)))
+                             )
+                     }
+                     )
+              )
+
+  ## extract the best likelihood for each dataset/model combination
+  best.ind <- tapply(1:nrow(x),list(x$dataset,x$model),function(k)k[which.max(x$loglik[k])])
+  best <- x[best.ind,]
+  best <- best[order(as.character(best$dataset)),]
+  write.csv(best,file=bestfile,row.names=FALSE,na="")
+
+  ## also save the MIF objects in a binary file
+  mle <- lapply(
+                results[noerr[best.ind]],
+                function(x)x$mle
+                )
+  names(mle) <- sapply(results[noerr[best.ind]],function(x)x$dataset)
+  mle <- mle[order(names(mle))]
+  save(list='mle',file=mlefile)
+}
 
 q(save='no',status=0)
