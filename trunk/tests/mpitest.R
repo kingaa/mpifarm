d664dce814581b7f280ef0b470da232898425e41
Aaron A. King <kingaa@umich.edu>
2009-05-12 20:02:22 +0000

    add some tests
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mpifarm@48 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/tests/mpitest.R
+++ trunk/tests/mpitest.R
@@ -3,24 +3,21 @@ mpi.spawn.Rslaves(needlog=T)
 
 require(mpifarm)
 
-x <- lapply(1:100,function(k)list(a=k,b=rnorm(1)))
-y <- mpi.farm(a+b,x)
-print(y)
+set.seed(1066)
+seeds <- ceiling(runif(n=mpi.universe.size(),0,2^31))
+x <- mpi.farm({set.seed(seed)},joblist=lapply(seeds,function(x)list(seed=x)),info=F)
 
-y <- mpi.farm({if(runif(1)<0.5)stop('yikes');a+b},x)
-print(y)
+x <- lapply(1:100,function(k)list(a=k,b=rnorm(1)))
+y <- mpi.farm(a+b,x,info=F)
 
 y <- mpi.farm({if(runif(1)<0.1)stop('yikes');a+b},x,info=F)
-print(y)
 warnings()
 
 x <- lapply(1:100,function(k)list(a=k,b=0,done=0))
-y <- mpi.farm({print(c(a,b,done));list(a=a,b=b+rnorm(1),done=done+1)},x,stop.condition=((abs(b)>2)|(done>10)))
-print(y)
+y <- mpi.farm({print(c(a,b,done));list(a=a,b=b+rnorm(1),done=done+1)},x,stop.condition=((abs(b)>2)|(done>10)),info=F)
 
 x <- lapply(1:300,function(k)list(a=k,b=10*k))
-y <- mpi.farm(a+mean(rnorm(n=b)),x,checkpoint.file="mpitest.rda",checkpoint=23)
-print(y)
+y <- mpi.farm(a+mean(rnorm(n=b)),x,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
 
 file.remove("mpitest.rda")
 
@@ -32,9 +29,9 @@ y <- mpi.farm(
               x,
               stop.condition=done,
               checkpoint.file="mpitest.rda",
-              checkpoint=500
+              checkpoint=500,
+              info=FALSE
               )
-print(y)
 
 file.remove("mpitest.rda")
 
---
+++ trunk/tests/simple.R
@@ -0,0 +1,31 @@
+require(Rmpi)
+mpi.spawn.Rslaves(needlog=T)
+require(mpifarm)
+nslave <- mpi.comm.size()-1
+nper <- 50
+nrand <- 1000
+njobs <- nper*nslave
+seeds <- ceiling(runif(njobs,0,2^31))
+job.list <- lapply(seeds,function(x)list(seed=x))
+tic <- Sys.time()
+job.result <- mpi.farm(
+	               {
+                         save.seed <- .Random.seed
+                         set.seed(seed)
+                         tic <- Sys.time()
+                         x <- rnorm(n)
+                         toc <- Sys.time()
+                         .Random.seed <<- save.seed
+                         me <- mpi.comm.rank()
+                         c(slave=me,time=toc-tic,mean=mean(x),sd=sd(x),n=length(x))
+                       },
+                       job.list,
+                       common=list(n=nrand),
+                       info=FALSE
+                       )
+toc <- Sys.time()
+x <- do.call(rbind,job.result)
+print(toc-tic)
+warnings()
+mpi.close.Rslaves()
+mpi.quit()
