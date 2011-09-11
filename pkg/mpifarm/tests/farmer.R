f508e6a2e27c518e237d4898f9af06c18838019a
Aaron A. King <kingaa@umich.edu>
2011-09-11 16:12:30 +0000

    - add test script for 'mpi.farmer'
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mpifarm@89 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mpifarm/tests/farmer.R
@@ -0,0 +1,29 @@
+library(mpifarm)
+
+mpifarm:::mpi.farmer(
+                     num=100,
+                     pre={
+                       seeds <- as.integer(ceiling(runif(n=num,min=0,max=2^31-1)))
+                       joblist <- list()
+                       for (k in seq_along(seeds))
+                         joblist[[k]] <- list(seed=seeds[k],a=k)
+                       joblist
+                     },
+                     main={
+                       save.seed <- .Random.seed
+                       set.seed(seed)
+                       x <- rnorm(n=1,mean=a+q)
+                       .Random.seed <<- save.seed
+                       list(s=a+q,x=x)
+                     },
+                     common=list(q=11),
+                     post={
+                       file.remove("farmer.rda")
+                       data.frame(
+                                  s=sapply(results,function(x)x$s),
+                                  x=sapply(results,function(x)x$x)
+                                  )
+                     },
+                     checkpoint.file="farmer.rda",
+                     checkpoint=1
+                     ) -> results
