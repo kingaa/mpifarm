3f46d1c45f36f751d38c783ecc31810febd0f9c7
Aaron A. King <kingaa@umich.edu>
2012-04-24 15:52:45 +0000

    - run unit tests only if Rmpi is available
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/mpifarm@119 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mpifarm/tests/mpitest.R
+++ pkg/mpifarm/tests/mpitest.R
@@ -1,94 +1,97 @@
-require(Rmpi)
-require(mpifarm)
+library(mpifarm)
 
-ncpus <- 8
+if (require(Rmpi)) {
 
-mpi.spawn.Rslaves(nslaves=ncpus,needlog=T)
+  ncpus <- 8
 
-set.seed(1066L)
-seeds <- as.integer(ceiling(runif(n=ncpus,0,2^31)))
+  mpi.spawn.Rslaves(nslaves=ncpus,needlog=T)
 
-x <- mpi.farm({set.seed(seed)},joblist=lapply(seeds,function(x)list(seed=x)),info=F)
+  set.seed(1066L)
+  seeds <- as.integer(ceiling(runif(n=ncpus,0,2^31)))
 
-x <- lapply(1:100,function(k)list(a=k,b=rnorm(1)))
-y <- mpi.farm(a+b,x,info=F)
+  x <- mpi.farm({set.seed(seed)},joblist=lapply(seeds,function(x)list(seed=x)),info=F)
 
-x <- lapply(1:100,function(k)list(a=k))
-y <- mpi.farm({for (j in 1:10000) j <- j+1; a},x,info=F)
-stopifnot(sum(diff(as.numeric(y))!=1)==0)
+  x <- lapply(1:100,function(k)list(a=k,b=rnorm(1)))
+  y <- mpi.farm(a+b,x,info=F)
 
-y <- mpi.farm({if(runif(1)<0.1)stop('yikes');a+runif(1)},x,info=F)
-warnings()
+  x <- lapply(1:100,function(k)list(a=k))
+  y <- mpi.farm({for (j in 1:10000) j <- j+1; a},x,info=F)
+  stopifnot(sum(diff(as.numeric(y))!=1)==0)
 
-seeds <- as.integer(ceiling(runif(n=321,1,2^31)))
-x <- lapply(1:321,function(k)list(a=k,b=10*k,seed=seeds[k]))
-y1 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=x,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-load("mpitest.rda")
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,status=status,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-stopifnot(identical(y1,y2))
+  y <- mpi.farm({if(runif(1)<0.1)stop('yikes');a+runif(1)},x,info=F)
+  warnings()
 
-joblist <- x
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
-y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  seeds <- as.integer(ceiling(runif(n=321,1,2^31)))
+  x <- lapply(1:321,function(k)list(a=k,b=10*k,seed=seeds[k]))
+  y1 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=x,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  load("mpitest.rda")
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,status=status,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  stopifnot(identical(y1,y2))
 
-y3 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,chunk=5,info=F,checkpoint=23,checkpoint.file="mpitest2.rda")
-y3 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,chunk=5,info=F,checkpoint=23,checkpoint.file="mpitest2.rda")
-stopifnot(identical(y2,y3))
+  joblist <- x
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
+  y2 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,checkpoint.file="mpitest.rda",checkpoint=23,info=F)
 
-x <- lapply(1:50,function(k)list(a=k,b=10*k,done=FALSE))
-y <- mpi.farm(
-              {
-               list(a=a-1,b=b,m=mean(rnorm(n=b)),done=(a<=1))
-               },
-              joblist=x,
-              stop.condition=done,
-              checkpoint.file="mpitest.rda",
-              checkpoint=500,
-              info=FALSE
-              )
+  y3 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,chunk=5,info=F,checkpoint=23,checkpoint.file="mpitest2.rda")
+  y3 <- mpi.farm({set.seed(seed); a+mean(rnorm(n=b))},joblist=joblist,chunk=5,info=F,checkpoint=23,checkpoint.file="mpitest2.rda")
+  stopifnot(identical(y2,y3))
 
-seeds <- as.integer(ceiling(runif(n=100,1,2^31)))
-x1 <- lapply(1:100,function(k)list(a=k,b=0,done=0,seed=seeds[k]))
-y1 <- mpi.farm(
-               {
-                 set.seed(seed)
-                 list(
-                      a=a,
-                      b=b+rnorm(1),
-                      done=done+1,
-                      seed=seed
-                      )
-               },
-               joblist=x1,
-               stop.condition=((abs(b)>2)|(done>10)),
-               info=F
-               )
+  x <- lapply(1:50,function(k)list(a=k,b=10*k,done=FALSE))
+  y <- mpi.farm(
+                {
+                  list(a=a-1,b=b,m=mean(rnorm(n=b)),done=(a<=1))
+                },
+                joblist=x,
+                stop.condition=done,
+                checkpoint.file="mpitest.rda",
+                checkpoint=500,
+                info=FALSE
+                )
 
-x2 <- lapply(1:100,function(k)list(a=k,b=rnorm(1),seed=seeds[k]))
-z1 <- unlist(mpi.farm({set.seed(seed); a+b},joblist=x2,info=F))
+  seeds <- as.integer(ceiling(runif(n=100,1,2^31)))
+  x1 <- lapply(1:100,function(k)list(a=k,b=0,done=0,seed=seeds[k]))
+  y1 <- mpi.farm(
+                 {
+                   set.seed(seed)
+                   list(
+                        a=a,
+                        b=b+rnorm(1),
+                        done=done+1,
+                        seed=seed
+                        )
+                 },
+                 joblist=x1,
+                 stop.condition=((abs(b)>2)|(done>10)),
+                 info=F
+                 )
 
-mpi.close.Rslaves()
+  x2 <- lapply(1:100,function(k)list(a=k,b=rnorm(1),seed=seeds[k]))
+  z1 <- unlist(mpi.farm({set.seed(seed); a+b},joblist=x2,info=F))
 
-y2 <- mpi.farm(
-               {
-                 set.seed(seed)
-                 list(
-                      a=a,
-                      b=b+rnorm(1),
-                      done=done+1,
-                      seed=seed
-                      )
-               },
-               joblist=x1,
-               stop.condition=((abs(b)>2)|(done>10)),
-               info=F
-               )
+  mpi.close.Rslaves()
 
-z2 <- unlist(mpi.farm({set.seed(seed); a+b},joblist=x2,info=F))
+  y2 <- mpi.farm(
+                 {
+                   set.seed(seed)
+                   list(
+                        a=a,
+                        b=b+rnorm(1),
+                        done=done+1,
+                        seed=seed
+                        )
+                 },
+                 joblist=x1,
+                 stop.condition=((abs(b)>2)|(done>10)),
+                 info=F
+                 )
 
-stopifnot(identical(y1,y2))
+  z2 <- unlist(mpi.farm({set.seed(seed); a+b},joblist=x2,info=F))
 
-stopifnot(identical(z1,z2))
+  stopifnot(identical(y1,y2))
+
+  stopifnot(identical(z1,z2))
+
+}
--- pkg/mpifarm/tests/mstop.R
+++ pkg/mpifarm/tests/mstop.R
@@ -1,112 +1,115 @@
-require(Rmpi)
-require(mpifarm)
+library(mpifarm)
 
-ncpus <- 6
+if (require(Rmpi)) {
 
-mpi.spawn.Rslaves(nslaves=ncpus,needlog=T)
+  ncpus <- 6
 
-njobs <- 200
+  mpi.spawn.Rslaves(nslaves=ncpus,needlog=T)
 
-set.seed(34588366L)
-seeds <- as.integer(ceiling(runif(n=njobs,min=0,max=2^31-1)))
-x <- list()
-for (i in seq_along(seeds)) {
-  x[[i]] <- list(seed=seeds[i],n=0,b=-1)
+  njobs <- 200
+
+  set.seed(34588366L)
+  seeds <- as.integer(ceiling(runif(n=njobs,min=0,max=2^31-1)))
+  x <- list()
+  for (i in seq_along(seeds)) {
+    x[[i]] <- list(seed=seeds[i],n=0,b=-1)
+  }
+
+  tic <- Sys.time()
+
+  y1 <- mpi.farm(
+                 {
+                   save.seed <- get(".Random.seed",envir=.GlobalEnv)
+                   if (n==0) set.seed(seed)
+                   else assign(".Random.seed",rng.state,envir=.GlobalEnv)
+                   b <- rnorm(1)
+                   n <- n+1
+                   rng.state <- get(".Random.seed",envir=.GlobalEnv)
+                   assign(".Random.seed",save.seed,envir=.GlobalEnv)
+                   list(
+                        seed=seed,
+                        rng.state=rng.state,
+                        b=b,
+                        n=n
+                        )
+                 },
+                 joblist=x,
+                 stop.condition=b[1]>0,
+                 chunk=1,
+                 info=FALSE
+                 )
+
+  toc <- Sys.time()
+  print(toc-tic)
+
+  tic <- Sys.time()
+
+  y2 <- mpi.farm(
+                 {
+                   save.seed <- get(".Random.seed",envir=.GlobalEnv)
+                   if (n==0) set.seed(seed)
+                   else assign(".Random.seed",rng.state,envir=.GlobalEnv)
+                   b <- rnorm(1)
+                   n <- n+1
+                   rng.state <- get(".Random.seed",envir=.GlobalEnv)
+                   assign(".Random.seed",save.seed,envir=.GlobalEnv)
+                   list(
+                        seed=seed,
+                        rng.state=rng.state,
+                        b=b,
+                        n=n
+                        )
+                 },
+                 joblist=x,
+                 stop.condition=b[1]>0,
+                 chunk=8,
+                 blocking=TRUE,
+                 info=FALSE
+                 )
+
+  toc <- Sys.time()
+  print(toc-tic)
+
+  tic <- Sys.time()
+
+  y3 <- mpi.farm(
+                 {
+                   save.seed <- get(".Random.seed",envir=.GlobalEnv)
+                   if (n==0) set.seed(seed)
+                   else assign(".Random.seed",rng.state,envir=.GlobalEnv)
+                   b <- rnorm(1)
+                   n <- n+1
+                   rng.state <- get(".Random.seed",envir=.GlobalEnv)
+                   assign(".Random.seed",save.seed,envir=.GlobalEnv)
+                   list(
+                        seed=seed,
+                        rng.state=rng.state,
+                        b=b,
+                        n=n
+                        )
+                 },
+                 joblist=x,
+                 stop.condition=b[1]>0,
+                 chunk=8,
+                 blocking=FALSE,
+                 info=FALSE
+                 )
+
+  toc <- Sys.time()
+  print(toc-tic)
+
+  mpi.close.Rslaves()
+
+  z1 <- sapply(y1,function(x)x$b)
+  n1 <- sapply(y1,function(x)x$n)
+
+  z2 <- sapply(y2,function(x)x$b)
+  n2 <- sapply(y2,function(x)x$n)
+
+  z3 <- sapply(y3,function(x)x$b)
+  n3 <- sapply(y3,function(x)x$n)
+
+  stopifnot(identical(z1,z2)&&identical(n1,n2))
+  stopifnot(identical(z2,z3)&&identical(n2,n3))
+
 }
-
-tic <- Sys.time()
-
-y1 <- mpi.farm(
-               {
-                 save.seed <- get(".Random.seed",envir=.GlobalEnv)
-                 if (n==0) set.seed(seed)
-                 else assign(".Random.seed",rng.state,envir=.GlobalEnv)
-                 b <- rnorm(1)
-                 n <- n+1
-                 rng.state <- get(".Random.seed",envir=.GlobalEnv)
-                 assign(".Random.seed",save.seed,envir=.GlobalEnv)
-                 list(
-                      seed=seed,
-                      rng.state=rng.state,
-                      b=b,
-                      n=n
-                      )
-               },
-               joblist=x,
-               stop.condition=b[1]>0,
-               chunk=1,
-               info=FALSE
-               )
-
-toc <- Sys.time()
-print(toc-tic)
-
-tic <- Sys.time()
-
-y2 <- mpi.farm(
-               {
-                 save.seed <- get(".Random.seed",envir=.GlobalEnv)
-                 if (n==0) set.seed(seed)
-                 else assign(".Random.seed",rng.state,envir=.GlobalEnv)
-                 b <- rnorm(1)
-                 n <- n+1
-                 rng.state <- get(".Random.seed",envir=.GlobalEnv)
-                 assign(".Random.seed",save.seed,envir=.GlobalEnv)
-                 list(
-                      seed=seed,
-                      rng.state=rng.state,
-                      b=b,
-                      n=n
-                      )
-               },
-               joblist=x,
-               stop.condition=b[1]>0,
-               chunk=8,
-               blocking=TRUE,
-               info=FALSE
-               )
-
-toc <- Sys.time()
-print(toc-tic)
-
-tic <- Sys.time()
-
-y3 <- mpi.farm(
-               {
-                 save.seed <- get(".Random.seed",envir=.GlobalEnv)
-                 if (n==0) set.seed(seed)
-                 else assign(".Random.seed",rng.state,envir=.GlobalEnv)
-                 b <- rnorm(1)
-                 n <- n+1
-                 rng.state <- get(".Random.seed",envir=.GlobalEnv)
-                 assign(".Random.seed",save.seed,envir=.GlobalEnv)
-                 list(
-                      seed=seed,
-                      rng.state=rng.state,
-                      b=b,
-                      n=n
-                      )
-               },
-               joblist=x,
-               stop.condition=b[1]>0,
-               chunk=8,
-               blocking=FALSE,
-               info=FALSE
-               )
-
-toc <- Sys.time()
-print(toc-tic)
-
-mpi.close.Rslaves()
-
-z1 <- sapply(y1,function(x)x$b)
-n1 <- sapply(y1,function(x)x$n)
-
-z2 <- sapply(y2,function(x)x$b)
-n2 <- sapply(y2,function(x)x$n)
-
-z3 <- sapply(y3,function(x)x$b)
-n3 <- sapply(y3,function(x)x$n)
-
-stopifnot(identical(z1,z2)&&identical(n1,n2))
-stopifnot(identical(z2,z3)&&identical(n2,n3))
